6828475b3b9a631d455b8019867d687a2d36fe4c
Jakub Garstka <jakub.garstka97@gmail.com>
2020-11-10 18:47:32 +0100

    SW.js added

--- src/App.js
+++ src/App.js
@@ -9,13 +9,12 @@ const App = () => {
 
     const search = async (e) => {
         if(e.key === 'Enter'){
-            const data = await fetchWeather(query)
+            const data = await fetchWeather(query).catch(err => console.log("Something went wrong :("))
             setWeather(data);
             setQuery('');    
         }
     }
 
-    
     return(
         <div className="main-container">
             <input type="text" className="search" placeholder="search..." value={query} onChange={(e) => setQuery(e.target.value)} onKeyPress={search}/>
--- src/api/fetchWeather.js
+++ src/api/fetchWeather.js
@@ -4,14 +4,13 @@ const URL = "https://api.openweathermap.org/data/2.5/forecast/";
 const API_KEY = process.env.REACT_APP_API_KEY;
 
 const fetchWeather = async (query) => {
-    const { data } = await  axios.get(URL, {
+    const { data } = await axios.get(URL, {
         params: {
             q: query,
             units: 'metric',
             APPID: API_KEY,
         }
-    });
-    console.log(data)
+    })
     return data;
 };
 
